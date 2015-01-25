cc69c09847742894935209e308e615aed6c64aef
Pietro Mercati <pietromercati@gmail.com>
2015-01-25 13:27:35 -0800

    prova2

--- core.c
+++ core.c
@@ -24,7 +24,39 @@
  *  2007-07-01  Group scheduling enhancements by Srivatsa Vaddagiri
  *  2007-11-29  RT balancing improvements by Steven Rostedt, Gregory Haskins,
  *              Thomas Gleixner, Mike Kravetz
- */
+ *  2015-01-25  Modified by Andrea Bartolini and Pietro Mercati to be 
+ * 		integrated in the Dynamic Reliability Management framework
+ *
+ *
+ * 		Modified by : Andrea Bartolini and Pietro Mercati
+ * 		email : pimercat@eng.ucsd.edu
+ * 
+ * 		If using this code for research purposes, include 
+ * 		references to the following publications
+ * 
+ * 		1) P.Mercati, A. Bartolini, F. Paterna, T. Rosing and L. Benini; A Linux-governor based 
+ *    		Dynamic Reliability Manager for android mobile devices. DATE 2014.
+ * 		2) P.Mercati, A. Bartolini, F. Paterna, L. Benini and T. Rosing; An On-line Reliability 
+ *    		Emulation Framework. EUC 2014
+ * 
+
+	This file is part of DRM.
+    	DRM is free software: you can redistribute it and/or modify
+    	it under the terms of the GNU General Public License as published by
+    	the Free Software Foundation, either version 3 of the License, or
+    	(at your option) any later version.
+
+    	DRM is distributed in the hope that it will be useful,
+    	but WITHOUT ANY WARRANTY; without even the implied warranty of
+    	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    	GNU General Public License for more details.
+
+    	You should have received a copy of the GNU General Public License
+    	along with DRM.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+
+
 #include <linux/mm.h>
 #include <linux/module.h>
 #include <linux/nmi.h>
--- monitor_infrastructure/monitor_infrastructure_userspace/monitor_userspace.c
+++ monitor_infrastructure/monitor_infrastructure_userspace/monitor_userspace.c
@@ -1,6 +1,36 @@
 
+// --------------- MONITOR INFRASTRUCTURE - USERSPACE DAEMON -----------------
+/*
+ * 
+ * Author: Andrea Bartolini
+ *
+ * Modified by : Pietro Mercati
+ * email : pimercat@eng.ucsd.edu
+ * 
+ * If using this code for research purposes, include 
+ * references to the following publications
+ * 
+ * 1) P.Mercati, A. Bartolini, F. Paterna, T. Rosing and L. Benini; A Linux-governor based 
+ *    Dynamic Reliability Manager for android mobile devices. DATE 2014.
+ * 2) P.Mercati, A. Bartolini, F. Paterna, L. Benini and T. Rosing; An On-line Reliability 
+ *    Emulation Framework. EUC 2014
+ * 
+	This file is part of DRM.
+        DRM is free software: you can redistribute it and/or modify
+        it under the terms of the GNU General Public License as published by
+        the Free Software Foundation, either version 3 of the License, or
+        (at your option) any later version.
+
+        DRM is distributed in the hope that it will be useful,
+        but WITHOUT ANY WARRANTY; without even the implied warranty of
+        MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+        GNU General Public License for more details.
+
+        You should have received a copy of the GNU General Public License
+        along with DRM.  If not, see <http://www.gnu.org/licenses/>.
 
 
+*/
 
 
 
@@ -12,57 +42,24 @@
 #include <sys/ioctl.h>
 #include <unistd.h>
 
-// Sched for affinity macros
-#define  __USE_GNU
-//#include <sched.h>
-
 #include <sched.h>
 #include <unistd.h>
 #include <string.h>
-
 #include <sys/syscall.h>
 #include <pthread.h>
-
 #include <sys/wait.h> //for wait()
-
-#define CPU_SETSIZE 1024
-#define __NCPUBITS  (8 * sizeof (unsigned long))
-typedef struct
-{
-   unsigned long __bits[CPU_SETSIZE / __NCPUBITS];
-} cpu_set_t;
-
-#define CPU_SET(cpu, cpusetp) \
-  ((cpusetp)->__bits[(cpu)/__NCPUBITS] |= (1UL << ((cpu) % __NCPUBITS)))
-#define CPU_ZERO(cpusetp) \
-  memset((cpusetp), 0, sizeof(cpu_set_t))
-
-
-/*
-define the structure of data that is passed
-it must be the same as the one defined in reliability.h
-*/
-struct reliability_stats_data {
-		unsigned long int cpu;
-		unsigned long int j; //jiffies
-		unsigned long int cycles;
-		unsigned long int instructions;
-};
 
 
 /*
 define the length of the buffer to export data
-the length must be the same of the one defined in reliability_stats_module.
+the length must be the same of the one defined in monitor_infrastructure_driver.
 */
-#define RELIABILITY_EXPORT_LENGTH 1024	
-#define NUM_CPU 4
 
 // ---------------- MAIN FUNCTION ----------------- //
 
-void main(int argc, char ** argv){
-	
-
-
+int main(){
+	printf("\n\nHello World !!\n\n");
+	return 0;
 }
 
 
